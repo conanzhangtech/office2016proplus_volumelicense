1a5de0cf42dd71ba0a9c3544bc5268b05f5edd4e
Conan <enquiriesZTC@outlook.com>
2021-04-25 16:56:46 +0800

    Update activate_kms.bat

--- activate_kms.bat
+++ activate_kms.bat
@@ -1,7 +1,7 @@
 color 0a
 cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
 cd /d %ProgramFiles%\Microsoft Office\Office16
-"for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x""
+for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
 cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
 cscript ospp.vbs /unpkey:BTDRB >nul
 cscript ospp.vbs /unpkey:KHGM9 >nul
